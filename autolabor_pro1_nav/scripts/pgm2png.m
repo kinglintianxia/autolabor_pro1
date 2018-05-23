d1f5e1d01e418a647688c68d0b451d4bb0156aa5
kinglintianxia <wangliujun_bit@163.com>
2018-05-23 23:31:09 +0800

    autolabor_pro1 navigation

---
+++ autolabor_pro1_nav/scripts/pgm2png.m
@@ -0,0 +1,2 @@
+img = imread('/home/king/auto_ws/src/autolabor_pro1/autolabor_pro1_nav/maps/lab_cartographer (copy).png')
+imwrite(img,'/home/king/auto_ws/src/autolabor_pro1/autolabor_pro1_nav/maps/lab_cartographer (copy).pgm')
